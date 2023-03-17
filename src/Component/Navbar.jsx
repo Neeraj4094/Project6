c1c51c406717063364533a49613fad0f1e598427
RaJ <4094_20neeraj@gc11.ac.in>
2023-03-17 12:48:07 +0530

    third save

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -21,7 +21,7 @@ export default function Nav1(){
       className="mr-3 h-6 sm:h-9"
       alt="Flowbite Logo"
       />
-    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white cursor-pointer">
       Flowbite
     </span>
   </Navbar.Brand>
