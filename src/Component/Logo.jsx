ee0247935b20693fa9ce2ce7ee3de673f617c063
Ankur <thedigitalakr@gmail.com>
2023-04-10 13:43:28 +0530

    six com

--- src/Component/Logo.jsx
+++ src/Component/Logo.jsx
@@ -10,49 +10,51 @@ function Logo() {
     <div className="flex flex-wrap max-w-5xl mx-auto -m-3">
       <div className="w-full md:w-1/2 lg:w-1/4 p-3">
         <div className="flex items-center justify-center py-8 px-9 h-full bg-white rounded-3xl">
-        <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAANdJREFUSEvtlOENgjAQhb9jAlIGkBHcQJ1ERnAEnUBHcAU3gAl0BP2vhgVMTUlDiBHaCI384H5dmst7797dVQgcEhifcRHEkArsBeZAaroXKB6wbHPCuwMDHsEZiJtggxEoOALrT6WDESSQa1gYAg0nDVkJpWtJvC1SFW4duydsXeB2Rj5lEIQggUzDzEqoFWvIBXLzHkFxt/k3qZ0WNX3v6LPTrt4EEax+7kDBwR6VmXC1QTZuAleTv2BTwqX3oQUZclPVROC8tsmi/1vkVNBS4P1dj5bgDUp4"/>
+        <a href=""><i class='bx bxl-facebook-circle'></i></a>
           <img src="flaro-assets/logos/brands/logo-xxl.png" alt="" />
         </div>
       </div>
       <div className="w-full md:w-1/2 lg:w-1/4 p-3">
         <div className="flex items-center justify-center py-8 px-9 h-full bg-white rounded-3xl">
-          <a href="/"><i className=' fs-1 bx bxl-apple' id='' ></i></a>
+          <a href=""><i className=' fs-1 bx bxl-apple' style={{
+            color:'black',
+          }} id='' ></i></a>
           <img src="flaro-assets/logos/brands/logo-xxl2.png" alt="" />
         </div>
       </div>
       <div className="w-full md:w-1/2 lg:w-1/4 p-3">
         <div className="flex items-center justify-center py-8 px-9 h-full bg-white rounded-3xl">
-          <a href="/"><i class='fs-1 bx bxl-microsoft' ></i></a>
+          <a href=""><i class='fs-1 bx bxl-microsoft' ></i></a>
           <img src="flaro-assets/logos/brands/logo-xxl3.png" alt="" />
         </div>
       </div>
       <div className="w-full md:w-1/2 lg:w-1/4 p-3">
         <div className="flex items-center justify-center py-8 px-9 h-full bg-white rounded-3xl">
-          <a href="/"><i class='fs-1 bx bxl-google' ></i></a>
+          <a href=""><i class='fs-1 bx bxl-google' ></i></a>
           <img src="flaro-assets/logos/brands/logo-xxl4.png" alt="" />
         </div>
       </div>
       <div className="w-full md:w-1/2 lg:w-1/4 p-3">
         <div className="flex items-center justify-center py-8 px-9 h-full bg-white rounded-3xl">
-          <a href="/"><i class='fs-1 bx bxl-trip-advisor' ></i></a>
+          <a href=""><i class='fs-1 bx bxl-trip-advisor' ></i></a>
           <img src="flaro-assets/logos/brands/logo-xxl5.png" alt="" />
         </div>
       </div>
       <div className="w-full md:w-1/2 lg:w-1/4 p-3">
         <div className="flex items-center justify-center py-8 px-9 h-full bg-white rounded-3xl">
-          <a href="/"><i class='fs-1 bx bxs-business' ></i></a>
+          <a href=""><i class='fs-1 bx bxs-business' ></i></a>
           <img src="flaro-assets/logos/brands/logo-xxl6.png" alt="" />
         </div>
       </div>
       <div className="w-full md:w-1/2 lg:w-1/4 p-3">
         <div className="flex items-center justify-center py-8 px-9 h-full bg-white rounded-3xl">
-          <a href="/"><i class=' fs-1 bx bx-code-curly' ></i></a>
+          <a href=""><i class=' fs-1 bx bx-code-curly' ></i></a>
           <img src="flaro-assets/logos/brands/logo-xxl7.png" alt="" />
         </div>
       </div>
       <div className="w-full md:w-1/2 lg:w-1/4 p-3">
         <div className="flex items-center justify-center py-8 px-9 h-full bg-white rounded-3xl">
-          <a href="/"><i class=' fs-1 bx bxl-github' ></i></a>
+          <a href=""><i class=' fs-1 bx bxl-github' ></i></a>
           <img src="flaro-assets/logos/brands/logo-xxl8.png" alt="" />
         </div>
       </div>
